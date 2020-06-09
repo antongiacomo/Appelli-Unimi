b1c642628c8ec61b167f1d9319596e1c50e5f48b
antongiacomo <antongiacomo@users.noreply.github.com>
2020-06-09 16:43:27 +0000

    deploy: 49232f3616b5f8d25547189c082953aa8fea6f70

---
+++ precache-manifest.b6906fbb28c155c8113adb51a97a7e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eb76475ca8d52ffe12d9bf4c2cf856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8edfc81178f17f2fd217",
+    "url": "/static/css/main.cf061eec.chunk.css"
+  },
+  {
+    "revision": "630e0ad742c9eae763eb",
+    "url": "/static/js/2.e960821a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e960821a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edfc81178f17f2fd217",
+    "url": "/static/js/main.eb776183.chunk.js"
+  },
+  {
+    "revision": "4e475950afdcf784d58a",
+    "url": "/static/js/runtime-main.7b1bf236.js"
+  }
+]);
