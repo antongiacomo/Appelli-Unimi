f510b2f52d261d32c0e9f8f7705ae9a6f18c3188
antongiacomo <antongiacomo@users.noreply.github.com>
2020-06-10 08:36:21 +0000

    deploy: 35ceed03222485fe43ceb7f113c1b609a3e99083

--- precache-manifest.6788a16048f3e7b8018d7290baafbfa9.js
+++ precache-manifest.6788a16048f3e7b8018d7290baafbfa9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "562c5c899020aab614c8783e3688e3e3",
+    "revision": "c1b9db8a492a43f8f5e10545a02c3a17",
     "url": "/Appelli-Unimi/index.html"
   },
   {
-    "revision": "f99646184edd480deaac",
+    "revision": "3281759f03de1a9a1f44",
     "url": "/Appelli-Unimi/static/css/main.cf061eec.chunk.css"
   },
   {
-    "revision": "630e0ad742c9eae763eb",
-    "url": "/Appelli-Unimi/static/js/2.e960821a.chunk.js"
+    "revision": "b708cab8b8269751522c",
+    "url": "/Appelli-Unimi/static/js/2.4a46617a.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/Appelli-Unimi/static/js/2.e960821a.chunk.js.LICENSE.txt"
+    "url": "/Appelli-Unimi/static/js/2.4a46617a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f99646184edd480deaac",
-    "url": "/Appelli-Unimi/static/js/main.52a47a5f.chunk.js"
+    "revision": "3281759f03de1a9a1f44",
+    "url": "/Appelli-Unimi/static/js/main.8b68a936.chunk.js"
   },
   {
     "revision": "a9948a0972e39a7ab025",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Appelli-Unimi/precache-manifest.8902ec8784e3dd489a32a0c135836181.js"
+  "/Appelli-Unimi/precache-manifest.6788a16048f3e7b8018d7290baafbfa9.js"
 );
 
 self.addEventListener('message', (event) => {
